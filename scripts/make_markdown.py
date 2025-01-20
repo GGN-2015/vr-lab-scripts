f18385deb0c7bb8ae754ee941e1eddb129d366ca
GGN_2015 <premierbob@qq.com>
2025-01-20 15:48:25 +0800

    feat:test

--- scripts/make_markdown.py
+++ scripts/make_markdown.py
@@ -15,6 +15,8 @@ general_terms = [
     "英文姓",
     "英文名",
     "是否是教师",
+    "身份", # 教师、学生、毕业生
+    "显示身份",
     "邮箱",
     "谷歌学术链接",
     "个人主页",
@@ -44,6 +46,84 @@ def get_all_json_file() -> list:
         if file.endswith(".json")
     ]
 
+# 计算用户所在分组
+def get_user_group(iden: str):
+    assert iden in ["教师", "学生", "毕业生"]
+    if iden == "教师":
+        return "Researchers"
+    
+    elif iden == "学生":
+        return "Grad Students"
+    
+    elif iden == "毕业生":
+        return "Alumni"
+    assert False
+
+# 生成 markdown 文件内容
+def gen_index_md_content(json_obj):
+    ans  = "---\n"
+
+    # 姓名信息
+    ans += "title: %s（%s）\n" % (json_obj["英文姓名"], json_obj["中文姓名"])
+    ans += "first_name: %s\n" % json_obj["英文名"]
+    ans += "last_name: %s\n" % json_obj["英文姓"]
+
+    # 是否是教师
+    is_teacher = ("true" if json_obj["是否是教师"] else "false")
+    ans += "superuser: %s\n" % is_teacher
+
+    # 身份填写
+    ans += "user_group: \n"
+    ans += "  - %s\n" % get_user_group(json_obj["身份"])
+
+    # 邮箱和谷歌学术
+    ans += "social: \n"
+    ans += "  - icon: envelope\n"
+    ans += "  - icon_pack: fas\n"
+    ans += "  - link: 'mailto:%s\n" % json_obj["邮箱"]
+    ans += "  - icon: google-scholar\n"
+    ans += "  - icon_pack: ai\n"
+    ans += "  - link: %s\n" % json_obj["谷歌学术链接"]
+
+    # 研究方向
+    if json_obj["研究方向"]:
+        ans += "interests:\n"
+        for term in  json_obj["研究方向"]:
+            ans += "  - %s\n" % term
+
+    # 填写角色身份
+    if json_obj["年级"] and json_obj["学位"]: # 例如：2020 级青岛研院硕士
+        role = "%d 级%s\n" % (json_obj["年级"], json_obj["学位"])
+    elif json_obj["显示身份"]:
+        role = json_obj["显示身份"]
+    else:
+        role = "" # 不显示身份
+    if role:
+        ans += "role: %s\n" % role
+
+    # 在作者列表中高亮老师的姓名吧
+    ans += "highlight_name: %s\n" % is_teacher
+
+    # 开始 Markdown 正文
+    ans += "---\n"
+    ans += "# %s\n" % json_obj["中文姓名"]
+    
+    if role:
+        ans += "%s\n" % role
+
+    # 介绍
+    for term_name in ["介绍", "个人履历", "教育背景", "科研经历", "毕业去向", "个人主页"]:
+        if json_obj[term_name]:
+            ans += "## %s\n" % term_name
+            ans += "%s\n" % json_obj[term_name]
+
+    return ans
+
+def gen_index_md(markdown_file, json_obj):
+    content = gen_index_md_content(json_obj)
+    with open(markdown_file, "w", encoding="utf-8")  as fp:
+        fp.write(content)
+
 # 读取一个 json 文件并且完成文件夹生成
 def gen_output_folder_based_on_json(json_file_path):
     with open(json_file_path) as fp:
@@ -53,16 +133,21 @@ def gen_output_folder_based_on_json(json_file_path):
 
     # 如果文件夹已经存在就把现存文件夹删除再创建新文件夹
     output_dir_now  = os.path.join(output_dir, file_index_name)
-    if os.path.isdir(output_dir_now):
-        print("    rmtree ...")
-        shutil.rmtree(output_dir_now)
     print("    makedirs ...")
+    if os.path.isdir(output_dir_now):
+        shutil.rmtree(output_dir_now)
     os.makedirs(output_dir_now, exist_ok=True)
 
-    # 检查图标是否存在
+    # 检查图标是否存在，如果存在就移动图标
     avatar = os.path.join(avatar_dir, "%s.jpg" % file_index_name)
-    print("    avatar ...")
+    print("    copy avatar ...")
     assert os.path.isfile(avatar)
+    shutil.copy2(avatar, os.path.join(output_dir_now, "avatar.jpg"))
+
+    # 生成 markdown 文件
+    print("    gen_index_md ...")
+    markdown_file = os.path.join(output_dir_now, "_index.md")
+    gen_index_md(markdown_file, json_obj)
 
 if __name__ == "__main__":
     for json_file_path in get_all_json_file():
